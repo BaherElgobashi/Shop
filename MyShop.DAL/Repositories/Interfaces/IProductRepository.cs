45e6a4c0ee11ecde5933501756ccb9589a28c18d
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-07-06 16:01:09 +0300

    Implement IProductRepository interface.

---
+++ MyShop.DAL/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using myshop.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DAL.Repositories.Interfaces
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAllProducts();
+
+        Product GetProductById(int id);
+        int Add(Product product);
+        int update(Product product);
+        int Delete(Product product);
+    }
+}
