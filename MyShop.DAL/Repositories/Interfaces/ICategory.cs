418b9afc5b142b30b5121a478eaf1b1b98d0927e
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-07-06 21:40:56 +0300

    Add Icategory Interface and implement the all signatures inside it.

---
+++ MyShop.DAL/Repositories/Interfaces/ICategory.cs
@@ -0,0 +1,18 @@
+﻿using myshop.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DAL.Repositories.Interfaces
+{
+    public interface ICategory
+    {
+        IEnumerable<Category> GetAllCategories();
+        Category GetCategoryById(int id);
+        int Add(Category category);
+        int Update(Category category);
+        int Delete(Category category);
+    }
+}
